dce3c9104e0ce4e0e2fc15a49885e4a65a090d85
Sohee Han <79372217+mynameis-sohee@users.noreply.github.com>
2021-04-01 11:07:17 +0900

    [9-1. 개인문제풀이] 01. 개인문제풀이.py
    기초 알고리즘 문제 복습
    - 팔린드롬 문제
    - 선형탐색 문제
    
    * 체감난이도 : 하

--- [9-1. 개인문제풀이] 01. 개인문제풀이.py
+++ [9-1. 개인문제풀이] 01. 개인문제풀이.py
@@ -16,6 +16,7 @@ def linear_search(element, some_list):
     for i in range(0,len(some_list)):
         if some_list[i] == element:
             return i
+    return None
 
 # 테스트
 print(linear_search(2, [2, 3, 5, 7, 11]))
